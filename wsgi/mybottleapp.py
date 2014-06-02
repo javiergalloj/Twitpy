c7a0d5ce7e7754824bae44c9ab833c7c00aa59f0
Javier Gallo Jiménez <javiergalloj@hotmail.com>
2014-06-02 15:06:11 +0200

    Actualizo archivo.

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -43,6 +43,7 @@ def ftimeline(oauth):
     jresp = json.loads (r.text)
     return jresp
 
+
 @route('/static/<filename>')
 def server_static(filename):
   return static_file(filename, root='./static')
